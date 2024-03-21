af5a2d7414c94f1dd3a7ab918d966c84db8ec7ef
User <User@DESKTOP-3V2P9PB>
2024-03-21 10:56:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Prochect.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Operations
+    {
+        public static int Max(int[] array)
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+
+        public static void Swap<T>(ref T x, ref T y) where T : struct
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static double SumOfFirstHalfDigits(int number)
+        {
+            int sum = 0;
+            string numStr = number.ToString();
+
+            foreach (var digit in numStr.Take(numStr.Length / 2))
+            {
+                sum += int.Parse(digit.ToString());
+            }
+
+            return sum;
+        }
+        
+    }
+
+
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+class MyMain
+{
+    public static void  Main(string[] args) {
+        int[] ints = { 1, 2, 3, 4, 5, 6, 11, 7};
+        for (int i = 0; i < ints.Length; i++)
+        {
+            Console.Write(ints[i].ToString() +  " ");
+        }
+        Console.WriteLine("\n" +  Operations.Max(ints));
+        Operations.Swap(ref ints[0], ref ints[6]);
+
+        for (int i = 0; i < ints.Length; i++)
+        {
+            Console.Write(ints[i].ToString() + " ");
+        }
+        Console.WriteLine("\n" +  Operations.SumOfFirstHalfDigits(1233));
+    }
+}
